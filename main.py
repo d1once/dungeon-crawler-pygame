a53ce2e2c30cccdd9a473679046a6f95a5e5bb30
d1once <dionisenja@gmail.com>
2022-09-12 08:43:45 +0200

    feat: loading data from CSV

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from re import I
 import pygame
+import csv
 import constants
 from character import Character
 from weapon import Weapon
@@ -14,6 +14,9 @@ pygame.display.set_caption("Dungeon Crawler")
 
 # create clock for maintaining frame rate
 clock = pygame.time.Clock()
+
+# define game variables
+level = 1
 
 # define player movement variables
 moving_left = False
@@ -120,13 +123,18 @@ def draw_info():
               constants.WHITE, constants.SCREEN_WIDTH - 100, 15)
 
 
-world_data = [
-    [7, 7, 7, 7, 7],
-    [7, 0, 1, 2, 7],
-    [7, 3, 4, 5, 7],
-    [7, 6, 6, 6, 7],
-    [7, 7, 7, 7, 7],
-]
+# create empty tile list
+world_data = []
+for row in range(constants.ROWS):
+    r = [-1] * constants.COLS
+    world_data.append(r)
+# load in level data and create world
+with open(f"levels/level{level}_data.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    for x, row in enumerate(reader):
+        for y, tile in enumerate(row):
+            world_data[x][y] = int(tile)
+
 
 world = World()
 world.process_data(world_data, tile_list)
