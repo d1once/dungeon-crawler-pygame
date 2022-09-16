b777cc03973f8c3198201acca4109bd6bc83eb51
d1once <dionisenja@gmail.com>
2022-09-16 08:17:38 +0200

    feat:collision

--- character.py
+++ character.py
@@ -22,7 +22,7 @@ class Character():
             0, 0, constants.TILE_SIZE * size, constants.TILE_SIZE * size)
         self.rect.center = (x, y)
 
-    def move(self, dx, dy):
+    def move(self, dx, dy, obstacle_tiles):
         screen_scroll = [0, 0]
         self.running = False
         if dx != 0 or dy != 0:
@@ -36,8 +36,27 @@ class Character():
             dx = dx * (math.sqrt(2)/2)
             dy = dy * (math.sqrt(2)/2)
 
+        # check for collision with map in x direction
         self.rect.x += dx
+        for obstacle in obstacle_tiles:
+            # check for collision
+            if obstacle[1].colliderect(self.rect):
+                # check which side the collision is from
+                if dx > 0:
+                    self.rect.right = obstacle[1].left
+                if dx < 0:
+                    self.rect.left = obstacle[1].right
+
+        # check for collision map in y direction
         self.rect.y += dy
+        for obstacle in obstacle_tiles:
+            # check for collision
+            if obstacle[1].colliderect(self.rect):
+                # check which side the collision is from
+                if dy > 0:
+                    self.rect.bottom = obstacle[1].top
+                if dy < 0:
+                    self.rect.top = obstacle[1].bottom
 
         # logic only applicable to player
         if self.char_type == 0:
